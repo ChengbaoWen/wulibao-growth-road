3eeff26c762122b5658454c8c4f58851ddf066e0
ChengbaoWen <1435468902@qq.com>
2020-05-22 18:33:03 +0800

    1.线程池使用练习 2.list转树形结构(递归)练习

--- src/main/java/com/wulibao/provider/node/NodeMain.java
+++ src/main/java/com/wulibao/provider/node/NodeMain.java
@@ -1,9 +1,10 @@
 package com.wulibao.provider.node;
 
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * list转树形结构
@@ -13,37 +14,51 @@ import java.util.List;
  */
 public class NodeMain {
     public static void main(String[] args) {
-        Node node1 = new Node(1L, 0L, "吉林省");
-        Node node2 = new Node(2L, 0L, "黑龙江省");
-        Node node3 = new Node(3L, 1L, "长春市");
-        Node node4 = new Node(4L, 2L, "哈尔滨市");
-        Node node5 = new Node(5L, 3L, "二道区");
-        Node node6 = new Node(6L, 3L, "经开区");
-        List<Node> list = new ArrayList<>();
+        Area node1 = new Area(1L, 0L, "吉林省");
+        Area node2 = new Area(2L, 0L, "黑龙江省");
+        Area node3 = new Area(3L, 1L, "长春市");
+        Area node4 = new Area(4L, 2L, "哈尔滨市");
+        Area node5 = new Area(5L, 3L, "二道区");
+        Area node6 = new Area(6L, 3L, "经开区");
+        List<Area> list = new ArrayList<>();
         list.add(node1);
         list.add(node2);
         list.add(node3);
         list.add(node4);
         list.add(node5);
         list.add(node6);
-        List<Node> nodes = buildTree(list);
+        List<Area> nodes = buildTree(list);
         System.out.println(nodes);
     }
     
-    // 把一个List转成树
-    static List<Node> buildTree(List<Node> list) {
-        List<Node> tree = new ArrayList<>();
-        for (Node node : list) {
-            if (node.getPid() == 0L) {
+    /**
+     * 转换成树
+     * 
+     * @param list list
+     * @param <T> <T>
+     * @return T
+     */
+    static <T extends Node<T>> List<T> buildTree(List<T> list) {
+        List<T> tree = new ArrayList<>();
+        for (T node : list) {
+            if (Long.valueOf(0L).equals(node.getParentId())) {
                 tree.add(findChild(node, list));
             }
         }
         return tree;
     }
     
-    static Node findChild(Node node, List<Node> list) {
-        for (Node n : list) {
-            if (n.getPid().equals(node.getId())) {
+    /**
+     * 递归方式找寻子节点
+     * 
+     * @param node node
+     * @param list list
+     * @param <T> <T>
+     * @return T
+     */
+    static <T extends Node<T>> T findChild(T node, List<T> list) {
+        for (T n : list) {
+            if (n.getParentId().equals(node.getId())) {
                 if (node.getNodes() == null) {
                     node.setNodes(new ArrayList<>());
                 }
@@ -54,21 +69,33 @@ public class NodeMain {
     }
     
     @Data
-    static class Node {
+    @EqualsAndHashCode(callSuper = true)
+    static class Area extends Node<Area> {
+        private String area;
         
+        public Area(Long id, Long parentId, String area) {
+            super.setId(id);
+            super.setParentId(parentId);
+            this.area = area;
+        }
+    }
+    
+    @Data
+    static class Node<T extends Node<T>> {
+        
+        /**
+         * 节点ID
+         */
         private Long id;
         
-        private Long pid;
+        /**
+         * 父节点ID
+         */
+        private Long parentId;
         
-        private String name;
-        
-        private List<Node> nodes;
-        
-        public Node(Long id, Long pid, String name) {
-            this.id = id;
-            this.pid = pid;
-            this.name = name;
-            this.nodes = new ArrayList<>();
-        }
+        /**
+         * 子节点
+         */
+        private List<T> nodes;
     }
 }
